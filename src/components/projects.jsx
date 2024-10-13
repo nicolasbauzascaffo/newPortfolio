3a9c922b0a923107388de2292cf94ed6631a598b
Nicolas Bauza <nicolasbauza21@gmail.com>
2024-10-13 01:02:20 -0300

    .

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -25,10 +25,10 @@ function Projects() {
         </section>
         <section className="projectBox">
           <p>Expo - IOS App</p>
-          <h2>Groove (only IOS)</h2>
+          <h2>Groove (IOS)</h2>
           <img src={music} alt="siteImg" />
           <Link
-            to="https://expo.dev/preview/update?message=Initial%20commit%0A%0AGenerated%20by%20create-expo-app%203.0.0.&updateRuntimeVersion=1.0.0&createdAt=2024-10-11T23%3A33%3A40.613Z&slug=exp&projectId=65a5875d-6d1d-4a6a-9272-c247b202effa&group=ef3686a5-ada3-46ff-8049-a6b7926f3793"
+            to="https://expo.dev/preview/update?message=Initial%20commit%0A%0AGenerated%20by%20create-expo-app%203.0.0.&updateRuntimeVersion=1.0.0&createdAt=2024-10-13T03%3A52%3A07.484Z&slug=exp&projectId=65a5875d-6d1d-4a6a-9272-c247b202effa&group=cad054d8-b627-4f28-9a48-9d3cbb2e7e5f"
             style={{ textDecoration: "none" }}
           >
             <button> Go to the App</button>
@@ -36,7 +36,7 @@ function Projects() {
         </section>
         <section className="projectBox">
           <p>Expo - IOS App</p>
-          <h2>StarBank (only IOS)</h2>
+          <h2>StarBank (IOS)</h2>
           <img src={bank} alt="siteImg" />
           <Link
             to="https://expo.dev/preview/update?message=Initial%20commit%0A%0AGenerated%20by%20create-expo-app%203.0.0.&updateRuntimeVersion=1.0.0&createdAt=2024-09-25T22%3A33%3A51.639Z&slug=exp&projectId=82b3e281-253f-47fc-985d-7dabca6624a1&group=e23bd010-757e-4213-91d2-d057dfe4f555"
