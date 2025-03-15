a36c89fa75e01f5a9d1fded7019e013b4a1bf1b9
Nicolas Bauza <nicolasbauza21@gmail.com>
2025-03-15 20:59:20 -0300

    se agregó nueva app al portfolio

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -6,6 +6,7 @@ import bank from '../images/bank.jpg'
 import { Link } from "react-router-dom";
 import music from '../images/musicImg.jpeg'
 import weather from '../images/weather_icon.png'
+import social_net from '../images/social_net.png'
 
 function Projects() {
   return (
@@ -13,6 +14,17 @@ function Projects() {
       <img className="rocketEmoji" src={rocketEmoji} alt="rocket" />
       <h2>My Projects</h2>
       <section className="projectsSection">
+      <section className="projectBox">
+          <p>Social Network</p>
+          <h2>Connectify</h2>
+          <img src={social_net} alt="siteImg" />
+          <Link
+            to="https://conn-red-social-app-sdrm.vercel.app/home"
+            style={{ textDecoration: "none" }}
+          >
+            <button> Go to the page</button>
+          </Link>
+        </section>
         <section className="projectBox">
           <p>E-commerce</p>
           <h2>EatRush</h2>
