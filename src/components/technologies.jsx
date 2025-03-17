616fc2bcbe716f136231f18166a6c0b8b4510182
Nicolas Bauza <nicolasbauza21@gmail.com>
2025-03-16 22:11:40 -0300

    se agregaron modificaciones al componente technologies

--- src/components/technologies.jsx
+++ src/components/technologies.jsx
@@ -1,22 +1,42 @@
 import React from "react";
 import techsEmoji from "../images/technologiesEmoji.png";
-import html from "../images/htmlLogo.png";
-import css from "../images/cssLogo.png";
 import js from "../images/JSlogo.png";
 import seq from "../images/sequelize.png";
 import react from "../images/ReactLogo.png";
 import redux from "../images/reduxLogo.png";
 import node from "../images/nodeLogo.png";
 import ex from "../images/expresLogo.png";
-import mySQl from "../images/mySQLlogo.png";
+import mysql from "../images/mySQLlogo.png";
 import ui from "../images/uiLogo.png";
 import mongo from "../images/mongo.png";
 import git from "../images/icons8-git-100.png";
 import ts from "../images/TS.png";
 import jest from "../images/jest-img.png";
 import gitHub from "../images/icons8-github-100.png";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { FreeMode, Pagination, Autoplay } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/free-mode";
+import "swiper/css/pagination";
 
 function Technologies() {
+  const technologies = [
+    { name: "JavaScript", image: js },
+    { name: "TypeScript", image: ts },
+    { name: "React", image: react },
+    { name: "React Native", image: react },
+    { name: "Redux", image: redux },
+    { name: "Node.js", image: node },
+    { name: "Express.js", image: ex },
+    { name: "MySQL", image: mysql },
+    { name: "MongoDB", image: mongo },
+    { name: "Git", image: git },
+    { name: "GitHub", image: gitHub },
+    { name: "Jest", image: jest },
+    { name: "Sequelize", image: seq },
+    { name: "Material UI", image: ui },
+  ];
+
   return (
     <div id="technologies" className="technologies">
       <img className="rocketEmoji" src={techsEmoji} alt="techs" />
@@ -27,63 +47,52 @@ function Technologies() {
           side, combining them in several personal projects.
         </p>
       </section>
-      <section className="technologiesSection">
-        <section className="techBox">
-          <img className="techIcon" src={js} alt="icon" />
-          <h5>JavaScript</h5>
-        </section>
-        <section className="techBox">
-          <img className="techIcon" src={ts} alt="icon" />
-          <h5>TypeScript</h5>
-        </section>
-        <section className="techBox">
-          <img className="techIcon" src={react} alt="icon" />
-          <h5>React</h5>
-        </section>
-        <section className="techBox">
-          <img className="techIcon" src={react} alt="icon" />
-          <h5>React Native</h5>
-        </section>
-        <section className="techBox">
-          <img className="techIcon" src={redux} alt="icon" />
-          <h5>Redux</h5>
-        </section>
-        <section className="techBox">
-          <img className="techIcon" src={node} alt="icon" />
-          <h5>Node.js</h5>
-        </section>
-        <section className="techBox">
-          <img className="techIcon" src={ex} alt="icon" />
-          <h5>Express.js</h5>
-        </section>
-        <section className="techBox">
-          <img className="techIcon" src={mySQl} alt="icon" />
-          <h5>MySQL</h5>
-        </section>
-        <section className="techBox">
-          <img className="techIcon" src={mongo} alt="icon" />
-          <h5>Mongo</h5>
-        </section>
-        <section className="techBox">
-          <img className="techIcon" src={git} alt="icon" />
-          <h5>Git</h5>
-        </section>
-        <section className="techBox">
-          <img className="techIcon" src={gitHub} alt="icon" />
-          <h5>GitHub</h5>
-        </section>
-        <section className="techBox">
-          <img className="techIcon" src={jest} alt="icon" />
-          <h5>Jest</h5>
-        </section>
-        <section className="techBox">
-          <img className="techIcon" src={seq} alt="icon" />
-          <h5>Sequelize</h5>
-        </section>
-        <section className="techBox">
-          <img className="techIcon" src={ui} alt="icon" />
-          <h5>Material UI</h5>
-        </section>
+      <section className="swipper_container" >
+        <Swiper
+          className="swipper_box"
+          autoplay={{
+            delay: 2500,
+            disableOnInteraction: false,
+          }}
+          modules={[FreeMode, Pagination, Autoplay]}
+          breakpoints={{
+            350: {
+              slidesPerView: 3,
+              spaceBetween: 1,
+            },
+            400: {
+              slidesPerView: 3,
+              spaceBetween: 1,
+            },
+            768: {
+              slidesPerView: 3,
+              spaceBetween: 1,
+            },
+            1024: {
+              slidesPerView: 4,
+              spaceBetween: 1,
+            },
+            1440: {
+              slidesPerView: 5,
+              spaceBetween: 1,
+            },
+          }}
+        >
+          {technologies.map((tech) => (
+            <SwiperSlide key={tech.name}>
+              <div className="techBox">
+                <img
+                  className="techIcon"
+                  src={tech.image}
+                  alt={`${tech.name} logo`}
+                />
+                <p style={{ fontWeight: "bold", fontSize: "13px" }}>
+                  {tech.name}
+                </p>
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
       </section>
     </div>
   );
