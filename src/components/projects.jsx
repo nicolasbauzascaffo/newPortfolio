e484fc76321339c9bc23cc92f54fe79b18687567
Nicolas Bauza <nicolasbauza21@gmail.com>
2025-06-05 13:54:38 -0300

    .

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -2,11 +2,10 @@ import React from "react";
 import rocketEmoji from "../images/rocketEmoji.png";
 import chartLogo from "../images/chart.jpg";
 import pokeball from "../images/pokeball.webp";
-import bank from '../images/bank.jpg'
+import bank from "../images/bank.jpg";
 import { Link } from "react-router-dom";
-import music from '../images/musicImg.jpeg'
-import weather from '../images/weather_icon.png'
-import social_net from '../images/social_net.png'
+import hotel from '../images/hotel.png'
+import social_net from "../images/social_net.png";
 
 function Projects() {
   return (
@@ -14,7 +13,18 @@ function Projects() {
       <img className="rocketEmoji" src={rocketEmoji} alt="rocket" />
       <h2>My Projects</h2>
       <section className="projectsSection">
-      <section className="projectBox">
+        <section className="projectBox">
+          <p style={{width:'40%'}} >Hotel reservations</p>
+          <h2>Zuno</h2>
+          <img src={hotel} alt="siteImg" />
+          <Link
+            to="https://conn-red-social-app-sdrm.vercel.app/home"
+            style={{ textDecoration: "none" }}
+          >
+            <button> Go to the page</button>
+          </Link>
+        </section>
+        <section className="projectBox">
           <p>Social Network</p>
           <h2>Connectify</h2>
           <img src={social_net} alt="siteImg" />
